1148f961b0293503f3c68888a68ca016dc1e72d2
Kjento-Lauwers <kjentolauwer@gmail.com>
2026-03-19 14:52:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
 const header = document.querySelector("header");
 
-/* scroll animatie sections */
+/* hamburger */
+const menuToggle = document.getElementById("menu-toggle");
+const nav = document.getElementById("nav");
+
+menuToggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
+
+/* scroll animatie */
 sections.forEach(section => {
     section.style.opacity = 0;
     section.style.transform = "translateY(40px)";
@@ -10,13 +19,28 @@ sections.forEach(section => {
 
 window.addEventListener("scroll", () => {
 
-    /* fade-in sections */
-    sections.forEach(section => {
-        const top = section.getBoundingClientRect().top;
+    let current = "";
 
-        if (top < window.innerHeight - 80) {
+    sections.forEach(section => {
+        const top = section.offsetTop - 100;
+
+        if (scrollY >= top) {
+            current = section.getAttribute("id");
+        }
+
+        /* animatie */
+        const visible = section.getBoundingClientRect().top;
+        if (visible < window.innerHeight - 80) {
             section.style.opacity = 1;
             section.style.transform = "translateY(0)";
+        }
+    });
+
+    /* actieve link */
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
         }
     });
 
