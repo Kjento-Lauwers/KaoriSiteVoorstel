0ef971cc88d7e52bf10f4870fb588ba204689382
Kjento-Lauwers <kjentolauwer@gmail.com>
2026-03-19 14:41:18 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const sections = document.querySelectorAll("section");
 
+sections.forEach(section => {
+    section.style.opacity = 0;
+    section.style.transform = "translateY(40px)";
+    section.style.transition = "all 0.6s ease";
+});
+
 window.addEventListener("scroll", () => {
     sections.forEach(section => {
         const top = section.getBoundingClientRect().top;
-        if (top < window.innerHeight - 100) {
+
+        if (top < window.innerHeight - 80) {
             section.style.opacity = 1;
             section.style.transform = "translateY(0)";
         }
     });
-});
-
-// start hidden
-sections.forEach(section => {
-    section.style.opacity = 0;
-    section.style.transform = "translateY(50px)";
-    section.style.transition = "all 0.6s ease";
 });
